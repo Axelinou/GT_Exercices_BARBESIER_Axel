238d460eb0bb5ef451cd5d3daaf5a9c0a40370d4
Axel.B <113009438+Axelinou@users.noreply.github.com>
2023-02-24 19:01:22 +0100

    Add GESTION_DE_FICHIER premier jet

--- GT_Exercices_Axel_Barbesier/GOLANG/GESTION_DE_FICHIERSTXT/GESTION_FICHIER.go
+++ GT_Exercices_Axel_Barbesier/GOLANG/GESTION_DE_FICHIERSTXT/GESTION_FICHIER.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+//IMPORTANT !!! Pensez a faire un "cd .\GOLANG\GESTION_DE_FICHIERTXT\" pour que le programme ce lance correctement. Merci
+//IMPORTANT N°2 !!! Veuillez lancer le programme via le terminal en tappant "go run .\GESTION_FICHIER.go" . Merci
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func write(text string, file *os.File) {
+	if _, err := file.WriteString(text); err != nil {
+		panic(err)
+	}
+}
+
+func read(filename string) string {
+	data, err := ioutil.ReadFile(filename)
+	check(err)
+	return string(data)
+}
+
+func main() {
+	file, err := os.OpenFile("text.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0600)
+	defer file.Close()
+	check(err)
+
+	write("Je suis en b1 info\n", file)
+
+	data := read(file.Name())
+	fmt.Print(data)
+}
